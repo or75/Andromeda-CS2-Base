0afac763a894cc0d0b538935f630d1176c1b75c7
xastrix <xxxxastrixxxx@gmail.com>
2025-10-30 23:20:22 +0300

    fix: forgot to call MH_RemoveHook()

--- Andromeda-CS2-Base/Andromeda-CS2-Base/DllMain.cpp
+++ Andromeda-CS2-Base/Andromeda-CS2-Base/DllMain.cpp
@@ -9,12 +9,10 @@ BOOL WINAPI DllMain( HINSTANCE hInstace , DWORD dwReason , LPVOID lpReserved )
 			DisableThreadLibraryCalls( hInstace );
 			GetDllLauncher()->OnDllMain( lpReserved , hInstace );
 
-			return TRUE;
 			break;
 		case DLL_PROCESS_DETACH:
 			GetDllLauncher()->OnDestroy();
 
-			return TRUE;
 			break;
 	}
 
